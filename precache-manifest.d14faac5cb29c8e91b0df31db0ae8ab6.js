8f7787e9ef7b6c4d41bdf0731ad242477f9aa10f
apple <uiyt48@livemail.tw>
2020-05-03 12:42:00 +0800

    靜態網頁

---
+++ precache-manifest.d14faac5cb29c8e91b0df31db0ae8ab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e10c26082a3bc1a5d79cbdd5d4f6d95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a48708b14f52eddfa55",
+    "url": "/static/css/2.97a2944b.chunk.css"
+  },
+  {
+    "revision": "36a874d7d82062e5adb4",
+    "url": "/static/css/main.24c6cb9e.chunk.css"
+  },
+  {
+    "revision": "5a48708b14f52eddfa55",
+    "url": "/static/js/2.6f3daa69.chunk.js"
+  },
+  {
+    "revision": "c471c91826a03f9cf1b71c65ec5e658b",
+    "url": "/static/js/2.6f3daa69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a874d7d82062e5adb4",
+    "url": "/static/js/main.9f893435.chunk.js"
+  },
+  {
+    "revision": "c54c5dea7a55b3d3dd0f",
+    "url": "/static/js/runtime-main.faddc3a4.js"
+  }
+]);
